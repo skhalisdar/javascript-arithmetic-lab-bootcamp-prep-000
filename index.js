e2c9354718d86a0ad578dcae7f958ca01428c1ee
Shafi Khalisdar <shafi.khalisdar@gmail.com>
2018-05-16 20:24:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
 function add(a, b) {
-    return (a + b);
+    n = a - b;
+    return n;
 };
 
-it('subtract(a, b) subtracts b from a and returns the result', function() {
-  expect(subtract(a, b)).toEqual(a - b)
-})
+function subtract(a, b) {
+  n = a - b;
+  return n;
+};
 
-it('multiply(a, b) multiplies two numbers and returns the result', function() {
-  expect(multiply(a, b)).toEqual(a * b)
-})
+function multiply(a, b) {
+  n = a * b;
+  return n;
+};
 
-it('divide(a, b) divides a by b and returns the result', function() {
-  expect(divide(a, b)).toEqual(a / b)
-})
+function divide (a, b) {
+  n = a / b;
+  return n;
+};
 
-it('inc(n) increments n and returns the result', function() {
-  expect(inc(a)).toEqual(a + 1)
-})
+function inc(n) {
+  n = n + 1;
+  return n;
+}
 
 it('dec(n) decrements n and returns the result', function() {
-  expect(dec(a)).toEqual(a - 1)
-})
+  return (a - 1)
+}
 
-describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
+function makeInt(n) {
+  function() {
+    return (makeInt(a.toString())).toEqual(a)
+  }
 
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
+   function() {
+     return makeInt('0x2328')).toEqual(0)
+  }
 
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
+function makeInt() {
+      makeInt('sldkjflksjf')
+  }
+}
 
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
+function preserveDecimal(n) {
+  function() {
+    preserveDecimal('2.222')).toBe(2.222)
+  }
 
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
+  function isNaN() {
+    (preserveDecimal('sldkjflksjf')
+  }
+}
